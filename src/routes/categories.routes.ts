e74e8d0816c8ac805b8e76749a54640000829077
Maurício <mauricio.caminha@hotmail.com>
2023-01-25 12:45:29 -0300

    routes, models and repositories

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
+
+const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
+
+// server.ts -> "/categories" = routes.ts -> path inicial categories
+categoriesRoutes.post("/", (request, response) => {
+    const { name, description } = request.body;
+
+    categoriesRepository.create({ name, description })
+
+    return response.status(201).json({ });
+});
+
+export { categoriesRoutes };
